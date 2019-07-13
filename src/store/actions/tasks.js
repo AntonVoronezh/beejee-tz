20cf9a6129b209c035c39980f1959154bf24b766
AntonVoronezh <pstut@mail.ru>
2019-07-13 15:41:27 +0300

    refactoring reducer

--- src/store/actions/tasks.js
+++ src/store/actions/tasks.js
@@ -1,115 +1,83 @@
-const FETCH_FILTER_REQUEST = 'FETCH_FILTER_REQUEST';
-const fetchFilterRequestAC = () => ({
-	type: FETCH_FILTER_REQUEST,
+const FETCH_TASKS_REQUEST = 'FETCH_TASKS_REQUEST';
+const fetchTasksRequestAC = () => ({
+	type: FETCH_TASKS_REQUEST,
 });
 
-const FETCH_FILTER_SUCCESS = 'FETCH_FILTER_SUCCESS';
-const fetchFilterSuccessAC = tasks => ({
-	type: FETCH_FILTER_SUCCESS,
+const FETCH_TASKS_SUCCESS = 'FETCH_TASKS_SUCCESS';
+const fetchTasksSuccessAC = tasks => ({
+	type: FETCH_TASKS_SUCCESS,
 	tasks,
 });
 
-const FETCH_FILTER_FAILURE = 'FETCH_FILTER_FAILURE';
-const fetchFilterFailureAC = errorMsg => ({
-	type: FETCH_FILTER_FAILURE,
+const FETCH_TASKS_FAILURE = 'FETCH_TASKS_FAILURE';
+const fetchTasksFailureAC = errorMsg => ({
+	type: FETCH_TASKS_FAILURE,
 	errorMsg,
 });
 
-const CHANGE_PAGINATION_COUNT = 'CHANGE_PAGINATION_COUNT';
-const changePaginationCountAC = () => ({
-	type: CHANGE_PAGINATION_COUNT,
-});
+// const CHANGE_PAGINATION_COUNT = 'CHANGE_PAGINATION_COUNT';
+// const changePaginationCountAC = () => ({
+// 	type: CHANGE_PAGINATION_COUNT,
+// });
 
 const fetchTasks = service => () => (dispatch, getState) => {
-	dispatch(fetchFilterRequestAC());
+	dispatch(fetchTasksRequestAC());
 
-	const { tasks: {pageNum} } = getState();
+	const { tasks: {pageNum, filter} } = getState();
 
 	service
-		.getTasks(pageNum)
+		.getTasks(pageNum, filter)
 		.then(data => {
 			const { status, tasks, message } = data;
 			
 			if (status === 'ok') {
-				dispatch(fetchFilterSuccessAC(tasks));
-				dispatch(changePaginationCountAC());
-			} else if (status === 'err') {
-				dispatch(fetchFilterFailureAC(message));
+				dispatch(fetchTasksSuccessAC(tasks));
+				// dispatch(changePaginationCountAC());
+			} else if (status === 'error') {
+				dispatch(fetchTasksFailureAC(message));
 			}
 		})
 
-		.catch(err => dispatch(fetchFilterFailureAC(err.message)));
+		.catch(err => dispatch(fetchTasksFailureAC(err.message)));
 };
 
-const FILTERED_ARR = 'FILTERED_ARR';
-const filteredArrAC = tasks => ({
-	type: FILTERED_ARR,
-	tasks,
-});
+// const FILTERED_ARR = 'FILTERED_ARR';
+// const filteredArrAC = tasks => ({
+// 	type: FILTERED_ARR,
+// 	tasks,
+// });
 
-const CHANGE_SELECT_COUNT = 'CHANGE_SELECT_COUNT';
-const changeSelectCountAC = count => ({
-	type: CHANGE_SELECT_COUNT,
-	count,
-});
+// const CHANGE_SELECT_COUNT = 'CHANGE_SELECT_COUNT';
+// const changeSelectCountAC = count => ({
+// 	type: CHANGE_SELECT_COUNT,
+// 	count,
+// });
 
-const CHANGE_COUNTED = 'CHANGE_COUNTED';
-const changeCountedAC = () => ({
-	type: CHANGE_COUNTED,
-});
+// const CHANGE_COUNTED = 'CHANGE_COUNTED';
+// const changeCountedAC = () => ({
+// 	type: CHANGE_COUNTED,
+// });
 
-const changeCount = count => (dispatch, getState) => {
-	dispatch(changeSelectCountAC(count));
-	dispatch(changePaginationCountAC());
-	dispatch(changeCountedAC());
-};
+// const changeCount = count => (dispatch, getState) => {
+// 	dispatch(changeSelectCountAC(count));
+// 	dispatch(changePaginationCountAC());
+// 	dispatch(changeCountedAC());
+// };
 
-const CHANGE_ACTIVE_PAGE_PAG = 'CHANGE_ACTIVE_PAGE_PAG';
-const changeActivePagePagAC = page => ({
-	type: CHANGE_ACTIVE_PAGE_PAG,
-	page,
-});
+// const CHANGE_ACTIVE_PAGE_PAG = 'CHANGE_ACTIVE_PAGE_PAG';
+// const changeActivePagePagAC = page => ({
+// 	type: CHANGE_ACTIVE_PAGE_PAG,
+// 	page,
+// });
 
-const changeActivePagePag = page => dispatch => {
-	dispatch(changeActivePagePagAC(page));
-	dispatch(changeCountedAC());
-};
-
-const filterTasks = value => (dispatch, getState) => {
-	const { tasks } = getState().tasks;
-
-	let result;
-
-	switch (value) {
-		case 'All':
-			result = tasks;
-			break;
-		case 'Active':
-			result = tasks.filter(el => el.active);
-			break;
-		case 'NonActive':
-			result = tasks.filter(el => !el.active);
-			break;
-		default:
-			result = tasks;
-	}
-
-	dispatch(changePaginationCountAC());
-	dispatch(filteredArrAC(result));
-};
+// const changeActivePagePag = page => dispatch => {
+// 	dispatch(changeActivePagePagAC(page));
+// 	dispatch(changeCountedAC());
+// };
 
 export {
 	fetchTasks,
-	FETCH_FILTER_REQUEST,
-	FETCH_FILTER_SUCCESS,
-	FETCH_FILTER_FAILURE,
-	filterTasks,
-	FILTERED_ARR,
-	CHANGE_SELECT_COUNT,
-	changeSelectCountAC,
-	CHANGE_COUNTED,
-	changeCount,
-	CHANGE_PAGINATION_COUNT,
-	CHANGE_ACTIVE_PAGE_PAG,
-	changeActivePagePag,
+	FETCH_TASKS_REQUEST,
+	FETCH_TASKS_SUCCESS, 
+	FETCH_TASKS_FAILURE
 };
