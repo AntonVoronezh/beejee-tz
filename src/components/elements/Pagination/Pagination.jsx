478c101c48084ab5c1ece834fa5faaabea191b64
AntonVoronezh <pstut@mail.ru>
2019-07-14 15:18:22 +0300

    add Pagination.propTypes = {

--- src/components/elements/Pagination/Pagination.jsx
+++ src/components/elements/Pagination/Pagination.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
+import PropTypes from 'prop-types';
 
-import './Pagination.css'
+import './Pagination.css';
 
 const Pagination = ({ tasksCount, pageNum, changeActivePage }) => {
 	const pageCount = Math.ceil(tasksCount / 3);
-	
+
 	const onPageChange = ({ selected }) => {
 		changeActivePage(selected + 1);
 	};
@@ -34,6 +35,12 @@ const Pagination = ({ tasksCount, pageNum, changeActivePage }) => {
 		) : null;
 
 	return <div>{pag}</div>;
+};
+
+Pagination.propTypes = {
+	tasksCount: PropTypes.number.isRequired,
+	pageNum: PropTypes.number.isRequired,
+	changeActivePage: PropTypes.func.isRequired,
 };
 
 export default Pagination;
